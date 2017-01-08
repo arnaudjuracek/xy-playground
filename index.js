248dd6961854f3caaa895fd1fac62b0735bffee5
RNO <arnaud.juracek@gmail.com>
2017-01-08 02:26:33 +0100

    Release flowfield-2

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ var plotter = require('xy-plotter')();
 
 // -------------------------------------------------------------------------
 
-function Agent(x, y, speed, scale) {
-  var pos = {x: x, y: y};
-  var vel = {x: 0, y: 0};
+function Agent(x, y, i) {
+  var pos = {x: x, y: y, offset: 0};
   var acc = {x: 0, y: 0};
 
   return {
-    update: function() {
-      var theta = mm.noise(pos.x / scale, pos.y / scale) * Math.PI;
-      acc.x = Math.sin(theta);
-      acc.y = Math.cos(theta);
+    update: function(scale, speed) {
+      var theta = mm.noise(pos.x / scale, pos.y / scale) * Math.PI * 2;
+      acc.x = Math.sin(theta + Math.PI / 2);
+      acc.y = Math.cos(theta + Math.PI / 2);
 
-      vel.x += acc.x * speed;
-      vel.y += acc.y * speed * 2;
-
-      pos.x += vel.x;
-      pos.y += vel.y;
+      pos.x += acc.x * speed;
+      pos.y += acc.y * speed;
 
       if (pos.x > 0 && pos.x < plotter.width && pos.y > 0 && pos.y < plotter.height) return pos;
       else return null;
@@ -36,28 +32,20 @@ var job = plotter.Job('flowfield');
 var cx = plotter.width / 2;
 var cy = plotter.height / 2;
 
-var margin = {x: 100, y: 50};
+var margin = {x: 50, y: 50};
 var size   = {x: plotter.width, y: plotter.height};
 
 // spawning agents
-var noiseScale = 40;
-var speed      = 0.4;
-var lineLength = 5;
-
-var circle = {
-  x: cx,
-  y: cy * 0.5,
-  r: 150
-};
+var noiseScale = {min: 200, max: 75};
+var speed      = {min: 10, max: 2};
 
 var agents = function() {
   var agents = [];
-  for (var i = 0; i < 10000; i++) {
+  for (var i = 0; i < 1000; i++) {
   agents[i] = new Agent(
                         mm.random(margin.x, size.x - margin.x),
                         mm.random(margin.y, size.y - margin.y),
-                        speed,
-                        noiseScale);
+                        i);
   }
   return agents;
 }();
@@ -66,15 +54,22 @@ var agents = function() {
 var lines = [];
 for (var index = 0, l = agents.length; index < l; index++) {
   var line = [];
-  for (var iteration = 0; iteration < lineLength; iteration++) {
-    var agent = agents[index];
-    var pos = agent.update();
-    if (pos) {
-      var radius = mm.random(circle.r * 0.9, circle.r * 1.1);
-      if (iteration < lineLength * 0.6 || distSq(pos.x, pos.y, circle.x, circle.y) < radius * radius) line.push([pos.x, pos.y]);
-      else break;
-    }
-    else break;
+  var agent = agents[index];
+  var running = true;
+  var px = 0;
+  var iter = 0;
+  while (running && iter++ < 10000) {
+    var t = mm.norm(px, 0, plotter.width);
+    var scale = mm.lerp(noiseScale.min, noiseScale.max, t);
+    var spd = mm.lerp(speed.min, speed.max, t);
+
+    var pos = agent.update(scale, spd);
+
+    var rnd = mm.random(-10, 10);
+    if (pos && pos.x >= margin.x && pos.x < size.x - margin.x + rnd && pos.y > margin.y + rnd && pos.y < size.y - margin.y + rnd) {
+      line.push([pos.x + pos.offset, pos.y]);
+      px = pos.x;
+    } else running = false;
   }
   lines.push(line);
 }
@@ -96,30 +91,6 @@ for (var i = lines.length - 1; i > 0; i--) {
 }
 console.log('lines.length', lines.length);
 
-// sorting lines
-console.log(lines[0][0]);
-// console.log('sorting on the x axis...');
-// bubbleSort(lines, function(line) { return line[0][0] });
-// console.log(lines[0][0]);
-console.log('sorting on the y axis...');
-bubbleSort(lines, function(line) { return line[0][1] });
-console.log(lines[0][0]);
-
-function bubbleSort(a, fn) {
-  var swapped;
-  do {
-    swapped = false;
-    for (var i=0; i < a.length-1; i++) {
-      if (fn(a[i]) > fn(a[i+1])) {
-        var temp = a[i];
-        a[i] = a[i+1];
-        a[i+1] = temp;
-        swapped = true;
-      }
-    }
-  } while (swapped);
-}
-
 // finally drawing
 for (var i = 0, l = lines.length; i < l; i++) {
   job.pen_up();
@@ -133,5 +104,5 @@ for (var i = 0, l = lines.length; i < l; i++) {
 // -------------------------------------------------------------------------
 
 console.log(plotter.Stats(job).getDuration().estimation.formatted);
-// plotter.File().export(job, path.join(__dirname, 'preview.png'));
-// plotter.Server('xy-server.local').queue(job, () => console.log('queued'));
+plotter.File().export(job, path.join(__dirname, 'preview.png'));
+plotter.Server('xy-server.local').queue(job, () => console.log('queued'));

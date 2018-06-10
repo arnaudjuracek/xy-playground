54aa4bfd7592d5801993866bd3b9d7e05d779848
RNO <arnaud.juracek@gmail.com>
2018-06-10 18:51:50 +0200

    Release crop-tex-3

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const TITLE = 'crop-tex-3'
+
 const path  = require('path')
 const plotter = require('xy-plotter')()
 
-const { radians } = require('missing-math')
+const { random, radians } = require('missing-math')
 const clip = require('lineclip')
-const job = plotter.Job('crop-tex-2')
 
+const job = plotter.Job(TITLE)
 
 const slope = (from, deg, len) => [
   from[0] + Math.cos(radians(deg)) * len,
@@ -18,13 +20,17 @@ const aabb = [
   plotter.height / 2 + 50
 ]
 
-for (let i = -100; i < 200; i++) {
+for (let i = -1; i < 99; i += 1.5) {
   const a = [aabb[0] + i, aabb[1]]
-  const b = slope(a, i, 200)
-  const lines = clip([a, b], aabb)
-  lines.forEach(line => job.line(...line[0], ...line[1]))
+  const b = slope(a, 90 - i, 500)
+
+  for (let j = 0; j < 5; j += 1.5) {
+    const lines = clip([[a[0] + j, a[1]], [b[0] - j, b[1]]], aabb)
+    lines.forEach(line => job.line(...line[0], ...line[1]))
+  }
 }
 
-console.log(plotter.Stats(job).duration.estimation.formatted)
+console.log('Estimated duration:', plotter.Stats(job).duration.estimation.formatted)
+
 plotter.File().export(job, path.join(__dirname, 'preview.png'))
 // plotter.Server('xy-server.local').queue(job, () => console.log('queued'))

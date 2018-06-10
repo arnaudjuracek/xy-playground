c83e331dd154e3a95f2412bd340f729ce92d3e08
RNO <arnaud.juracek@gmail.com>
2018-06-10 20:00:23 +0200

    Improve playground ENV

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 ![preview](preview.png?raw=true "preview")
 
+### Usage
+
+###### preview a job
+
+```sh
+$ yarn [preview|start] # or ST3/VSCode node build on index.js
+```
+
+###### queue a job to [`xy-server`](https://github.com/xy-plotter/xy-server)
+
+```sh
+$ yarn queue
+```
+
+###### release a job
+Use [`git release <TITLE>`](https://gist.github.com/arnaudjuracek/318872b3029f0e0de1555fd232e2f58c#git-aliases)</sup>, or:
+```sh
+$ git commit -am "Release <TITLE>" 
+$ git tag -a -m "<TITLE>"
+```
+
 ### References
 
-- [arnaudjuracek/xy](https://github.com/arnaudjuracek/xy)
-- [arnaudjuracek/xy-server](https://github.com/arnaudjuracek/xy-server)
+- [xy-plotter/xy](https://github.com/xy-plotter/xy)
+- [xy-plotter/xy-server](https://github.com/xy-plotter/xy-server)
 - [arnaudjuracek/missing-math](https://github.com/arnaudjuracek/missing-math)
 
 ### License
--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-const TITLE = 'crop-tex-3'
+const playground = require('./lib/playground')
 
-const path  = require('path')
-const plotter = require('xy-plotter')()
+playground({
+  title: 'crop-tex-3',
+  server: 'xy-server.local'
+}, ({ plotter, job }) => {
+  const { radians } = require('missing-math')
+  const clip = require('lineclip')
 
-const { random, radians } = require('missing-math')
-const clip = require('lineclip')
+  const slope = (from, deg, len) => [
+    from[0] + Math.cos(radians(deg)) * len,
+    from[1] + Math.sin(radians(deg)) * len
+  ]
 
-const job = plotter.Job(TITLE)
+  const aabb = [
+    plotter.width / 2 - 50,
+    plotter.height / 2 - 50,
+    plotter.width / 2 + 50,
+    plotter.height / 2 + 50
+  ]
 
-const slope = (from, deg, len) => [
-  from[0] + Math.cos(radians(deg)) * len,
-  from[1] + Math.sin(radians(deg)) * len,
-]
-
-const aabb = [
-  plotter.width / 2 - 50,
-  plotter.height / 2 - 50,
-  plotter.width / 2 + 50,
-  plotter.height / 2 + 50
-]
-
-for (let i = -1; i < 99; i += 1.5) {
-  const a = [aabb[0] + i, aabb[1]]
-  const b = slope(a, 90 - i, 500)
-
-  for (let j = 0; j < 5; j += 1.5) {
-    const lines = clip([[a[0] + j, a[1]], [b[0] - j, b[1]]], aabb)
-    lines.forEach(line => job.line(...line[0], ...line[1]))
+  for (let i = -1; i < 99; i += 1.5) {
+    const a = [aabb[0] + i, aabb[1]]
+    const b = slope(a, 90 - i, 500)
+    for (let j = 0; j < 5; j += 1.5) {
+      const lines = clip([[a[0] + j, a[1]], [b[0] - j, b[1]]], aabb)
+      lines.forEach(line => job.line(...line[0], ...line[1]))
+    }
   }
-}
-
-console.log('Estimated duration:', plotter.Stats(job).duration.estimation.formatted)
-
-plotter.File().export(job, path.join(__dirname, 'preview.png'))
-// plotter.Server('xy-server.local').queue(job, () => console.log('queued'))
+})

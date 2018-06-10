d47c4bf55be34befd1fd2772d02398e7b2fb26fa
RNO <arnaud.juracek@gmail.com>
2018-06-10 13:12:00 +0200

    Release crop-tex-1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const mm  = require('missing-math')
 const path  = require('path')
 const plotter = require('xy-plotter')()
+
+const { min, max, radians } = require('missing-math')
+const clip = require('lineclip')
 
 const server = plotter.Server('xy-server.local')
 const file = plotter.File()
@@ -9,6 +11,26 @@ const filename = path.join(__dirname, 'preview.png')
 const job = plotter.Job('crop-tex-1')
 
 
+const slope = (from, deg, len) => [
+  from[0] + Math.cos(radians(deg)) * len,
+  from[1] + Math.sin(radians(deg)) * len,
+]
+
+const aabb = [
+  plotter.width / 2 - 50,
+  plotter.height / 2 - 50,
+  plotter.width / 2 + 50,
+  plotter.height / 2 + 50
+]
+
+// job.rect(aabb[0], aabb[1], aabb[2] - aabb[0], aabb[3] - aabb[1])
+
+for (let i = -100; i < 200; i++) {
+  const a = [aabb[0] + i, aabb[1]]
+  const b = slope(a, 89, 200)
+  const lines = clip([a, b], aabb)
+  lines.forEach(line => job.line(...line[0], ...line[1]))
+}
 
 console.log(plotter.Stats(job).duration.estimation.formatted)
 

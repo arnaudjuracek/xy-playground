1adedd74a376abea8f3175118792463b8d0c030f
RNO <arnaud.juracek@gmail.com>
2018-06-10 18:49:09 +0200

    Release crop-tex-2

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const path  = require('path')
 const plotter = require('xy-plotter')()
 
-const { min, max, radians } = require('missing-math')
+const { radians } = require('missing-math')
 const clip = require('lineclip')
-
-const server = plotter.Server('xy-server.local')
-const file = plotter.File()
-
-const filename = path.join(__dirname, 'preview.png')
-const job = plotter.Job('crop-tex-1')
+const job = plotter.Job('crop-tex-2')
 
 
 const slope = (from, deg, len) => [
@@ -23,16 +18,13 @@ const aabb = [
   plotter.height / 2 + 50
 ]
 
-// job.rect(aabb[0], aabb[1], aabb[2] - aabb[0], aabb[3] - aabb[1])
-
 for (let i = -100; i < 200; i++) {
   const a = [aabb[0] + i, aabb[1]]
-  const b = slope(a, 89, 200)
+  const b = slope(a, i, 200)
   const lines = clip([a, b], aabb)
   lines.forEach(line => job.line(...line[0], ...line[1]))
 }
 
 console.log(plotter.Stats(job).duration.estimation.formatted)
-
-file.export(job, filename)
-server.queue(job, () => console.log('queued'))
+plotter.File().export(job, path.join(__dirname, 'preview.png'))
+// plotter.Server('xy-server.local').queue(job, () => console.log('queued'))
